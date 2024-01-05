0899563915d8025a7b3a6dc8396369aa35f9b907
Rely Ky <relyky@hotmail.com>
2024-01-05 12:45:53 +0800

    GraphQL client

--- N6GraphClient/Program.cs
+++ N6GraphClient/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+//## from GraphQL client
+builder.Services.AddN6GraphQLabClient()
+       .ConfigureHttpClient(cfg => cfg.BaseAddress = new Uri("https://localhost:7094/graphql/"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
